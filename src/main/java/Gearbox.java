00d4ae8e1c011b169146bea6d2721a2faf64d4a2
Ewan Brain <ewan.brain@gmail.com>
2021-02-16 14:17:07 +0000

    gearbox getters working

--- src/main/java/Gearbox.java
+++ src/main/java/Gearbox.java
@@ -7,4 +7,20 @@ public class Gearbox {
         this.gearboxType = gearboxType;
         this.numberOfGears = numberOfGears;
     }
+
+    public String getGearboxType() {
+        return gearboxType;
+    }
+
+    public void setGearboxType(String gearboxType) {
+        this.gearboxType = gearboxType;
+    }
+
+    public int getNumberOfGears() {
+        return numberOfGears;
+    }
+
+    public void setNumberOfGears(int numberOfGears) {
+        this.numberOfGears = numberOfGears;
+    }
 }
--- src/test/java/GearboxTest.java
+++ src/test/java/GearboxTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GearboxTest {
+
+    Gearbox gearbox;
+
+    @Before
+    public void before(){
+        gearbox = new Gearbox("Manual", 5);
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals ("Manual", gearbox.getGearboxType());
+    }
+
+    @Test
+    public void hasNumberOfGears() {
+        assertEquals(5, gearbox.getNumberOfGears());
+    }
 }
