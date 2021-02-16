902fb12ccef4fdea565d8feed26bc61804663873
Ewan Brain <ewan.brain@gmail.com>
2021-02-16 13:57:19 +0000

    electric car and petrol car set up

---
+++ src/main/java/ElectricCar.java
@@ -0,0 +1,7 @@
+public class ElectricCar extends Car {
+
+    public ElectricCar (String name, double price, String colour) {
+        super(name, price, colour);
+    }
+
+}
---
+++ src/main/java/PetrolCar.java
@@ -0,0 +1,7 @@
+public class PetrolCar extends Car {
+
+    public PetrolCar (String name, double price, String colour) {
+        super(name, price, colour);
+    }
+
+}
